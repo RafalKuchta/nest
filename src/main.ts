d17fc4a713c19c332f056c51607a75d31c9d4885
Rafal <40246937+RafalKuchta@users.noreply.github.com>
2022-11-04 12:21:21 +0100

    Edited cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  app.setGlobalPrefix('app');
+  
+  app.enableCors({
+    origin: 'https://nest.networkmanager.pl',
+    credentials: true,
+    optionsSuccessStatus: 200,
+  });
   const app = await NestFactory.create(AppModule);
   await app.listen(3002);
 }
